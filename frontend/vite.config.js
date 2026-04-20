8db435ed10811420d32aa875bcf460412caf326a
swastik-diff <mannaswastik686@gmail.com>
2026-04-21 02:02:43 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/api': 'https://intrusense-production-fc94.up.railway.app,
+      '/api': 'https://intrusense-production-fc94.up.railway.app',
       '/socket.io': {
         target: 'https://intrusense-production-fc94.up.railway.app',
         ws: true,
